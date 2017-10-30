fe86dd259cb90715a07b59a618ac16c55d568413
Ingrid Medeiros <i.smedeiros@hotmail.com>
2017-10-30 21:34:40 -0200

    Diretivas, Propriedades e iniciando criação do menu (AULA 6)

--- mini-ecommerce-webschool/src/App.vue
+++ mini-ecommerce-webschool/src/App.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="phone-viewport">
     <md-toolbar>
+      <md-button class="md-icon button" v-on:click.native="toggleLeftSide">
+        <md-icon>menu</md-icon>
+      </md-button> 
       <h1 class="md-title">{{title}}</h1>
-      <md-input-container md-inline>
-        <md-input v-model="title" placeholder="Digite aqui"></md-input>
-      </md-input-container>
     </md-toolbar>
+
+    <md-sidenav class="md-left" ref="leftSidenav">
+        <md-toolbar class="md-medium">
+          <div class="md-toolbar-container">
+            <md-button class="md-icon button" @click.native="toggleLeftSidenav">
+              <md-icon>menu</md-icon>
+            </md-button>
+            <h2 class="md-title">Sidenav content</h2>
+          </div>
+        </md-toolbar>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce viverra magna at urna semper, quis aliquet purus vestibulum. Etiam aliquet arcu nibh, imperdiet placerat lorem pharetra non. Aenean faucibus nisi nisi, dapibus facilisis ligula pretium sit amet.</p>
+    </md-sidenav>
   </div>
 </template>
 
@@ -13,10 +25,17 @@
 
 export default {
   name: 'app',
+
   data() {
     return {
-      title: ''
+      title: 'E-commerce'
     } 
+  },
+
+  methods:{
+    toggleLeftSide: function() {
+      this.$refs.leftSidenav.toggle()
+    }
   }
 }
 </script>
