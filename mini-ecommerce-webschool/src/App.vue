39db958c541f941722c08493452e5bdd6ee6789e
Ingrid Medeiros <i.smedeiros@hotmail.com>
2017-10-31 22:03:47 -0200

    Vue Router (AULA 07)

--- mini-ecommerce-webschool/src/App.vue
+++ mini-ecommerce-webschool/src/App.vue
@@ -7,17 +7,32 @@
       <h1 class="md-title">{{title}}</h1>
     </md-toolbar>
 
-    <md-sidenav class="md-left" ref="leftSidenav">
+    <md-sidenav class="md-left" ref="leftSidenav" @open="open('Left')" @close="close('Left')">
         <md-toolbar class="md-medium">
           <div class="md-toolbar-container">
             <md-button class="md-icon button" @click.native="toggleLeftSidenav">
               <md-icon>menu</md-icon>
             </md-button>
-            <h2 class="md-title">Sidenav content</h2>
+            <h2 class="md-title">Navegação</h2>
           </div>
         </md-toolbar>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce viverra magna at urna semper, quis aliquet purus vestibulum. Etiam aliquet arcu nibh, imperdiet placerat lorem pharetra non. Aenean faucibus nisi nisi, dapibus facilisis ligula pretium sit amet.</p>
+        
+        <md-list @click.native="closeLeftSidenav">
+          <md-list-item>Perfil</md-list-item>
+          <md-list-item>
+            <router-link to="/categories">Categorias</router-link>
+          </md-list-item>
+          <md-list-item>
+            <router-link to="/products">Produtos</router-link>
+          </md-list-item>
+          <md-list-item>Sair</md-list-item>
+        </md-list>
     </md-sidenav>
+
+    <md-row>
+      <router-view></router-view>
+    </md-row> 
+  
   </div>
 </template>
 
@@ -35,6 +50,18 @@ export default {
   methods:{
     toggleLeftSide: function() {
       this.$refs.leftSidenav.toggle()
+    },
+
+    closeLeftSidenav () {
+      this.$refs.leftSidenav.close()
+    },
+
+    open (ref) {
+      console.log('Menu aberto: ' + ref)
+    },
+
+    close (ref) {
+      console.log('Menu fechado: ' + ref)
     }
   }
 }
--- mini-ecommerce-webschool/src/main.js
+++ mini-ecommerce-webschool/src/main.js
@@ -1,11 +1,26 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css'
+
 import App from './App'
+import Categories from './components/Categories.vue'
+import Products from './components/Products.vue'
 
 Vue.use(VueMaterial)
+Vue.use(VueRouter)
+
+const routes = [
+	{ path: '/products' , component: Products } ,
+	{ path: '/categories' , component: Categories }
+]
+
+const router = new VueRouter({
+  	routes
+})
 
 Vue.material.registerTheme('default', {
   primary: 'blue',
@@ -15,7 +30,6 @@ Vue.material.registerTheme('default', {
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
-})
+  router,
+  render: (h) => h(App)
+}).$mount('#app')
