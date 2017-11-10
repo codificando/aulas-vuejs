a9d137eac3d006ed54a1c5bbf1ebb35412a2132f
Ingrid Medeiros <i.smedeiros@hotmail.com>
2017-11-10 21:53:10 -0200

    Lidando com colecões - v-for

--- aulas-schoolofnet/vue2/src/main.js
+++ aulas-schoolofnet/vue2/src/main.js
@@ -6,7 +6,16 @@ import {Time} from './time'
 new Vue({
   el: '#app',
   data: {
-    time: new Time('America MG' , require('./assets/america_mg_60x60.png'))
+  	times: [
+  		new Time('America MG' , require('./assets/america_mg_60x60.png')),
+  		new Time('Atlético PR' , require('./assets/atletico-pr_60x60.png')),
+  	],
+  	alfabeto: {
+  		a: 'A',
+  		b: 'B',
+  		c: 'C',
+  		d: 'D',
+  	}
   }
   // render: h => h(App)
 })
